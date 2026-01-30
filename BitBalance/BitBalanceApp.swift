b6a50d9f2fd938d4da4aff2f7a7d1736d1e21302
Mateus Sfeir <mateussfeir@hotmail.com>
2026-01-29 19:31:50 -0500

    Forced dark mode

--- BitBalance/BitBalanceApp.swift
+++ BitBalance/BitBalanceApp.swift
@@ -1,17 +1,12 @@
-//
-//  BitBalanceApp.swift
-//  BitBalance
-//
-//  Created by Mateus Sfeir on 2026-01-29.
-//
-
 import SwiftUI
+
 
 @main
 struct BitBalanceApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .preferredColorScheme(.dark)
         }
     }
 }
